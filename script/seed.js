d9ed5783dce2b9b3a4d1d0c7478aa8af29c9e130
Sey <76843977+iseykim@users.noreply.github.com>
2021-06-15 14:34:31 -0400

    working

--- script/seed.js
+++ script/seed.js
@@ -1,48 +1,62 @@
-'use strict';
+'use strict'
 
 const {
-  db,
-  models: { User, Product },
-} = require('../server/db');
+	db,
+	models: { User, Product }
+} = require('../server/db')
 
 /**
  * seed - this function clears the database, updates tables to
  *      match the models, and populates the database.
  */
 async function seed() {
-  await db.sync({ force: true }); // clears db and matches models to tables
-  console.log('db synced!');
+	await db.sync({ force: true }) // clears db and matches models to tables
+	console.log('db synced!')
 
-  // Creating Users
-  const [cody, murphy] = await Promise.all([
-    User.create({ username: 'cody', password: '123' }),
-    User.create({ username: 'murphy', password: '123' }),
-  ]);
+	// Creating Users
+	const [cody, murphy, sey, jason] = await Promise.all([
+		User.create({ username: 'cody', password: '123' }),
+		User.create({ username: 'murphy', password: '456' }),
+		User.create({ username: 'sey', password: 'abc' }),
+		User.create({ username: 'jason', password: 'def' })
+	])
 
-  // Creating Products
-  const [jackDaniels, greyGoose] = await Promise.all([
-    Product.create({ name: 'Jack Daniels', description: 'Whiskey' }),
-    Product.create({ name: 'Grey Goose', description: 'Vodka' }),
-  ]);
+	// Creating Products
+	const [jackDaniels, greyGoose, cham, mak, fire] = await Promise.all([
+		Product.create({ name: 'Jack Daniels', description: 'Whiskey' }),
+		Product.create({ name: 'Grey Goose', description: 'Vodka' }),
+		Product.create({ name: 'Cham', description: 'Soju' }),
+		Product.create({ name: 'Mak', description: 'Makgali' }),
+		Product.create({ name: 'Fire', description: 'Eww' })
+	])
 
-  // Magic Methods
-  await cody.setProducts(jackDaniels, { through: { quantity: 4 } });
-  await murphy.setProducts(greyGoose, { through: { quantity: 4 } });
+	// Magic Methods
+	await cody.addProduct(jackDaniels, { through: { quantity: 2 } })
+	await cody.addProduct(greyGoose, { through: { quantity: 15 } })
+	await murphy.setProducts(greyGoose, { through: { quantity: 5 } })
+	// TEST OUT OUR QUERIES HERE
 
-  // // Creating CartItem
-  // const users = await Promise.all([
-  //   User.create({ username: 'cody', password: '123' }),
-  //   User.create({ username: 'murphy', password: '123' }),
-  // ]);
+	// const results = await cody.getProducts()
+	// const result = await User.findOne({
+	// 	where: { username: 'cody' },
+	// 	include: Product
+	// })
 
-  // console.log(`seeded ${users.length} users`);
-  // console.log(`seeded successfully`);
-  // return {
-  //   users: {
-  //     cody: users[0],
-  //     murphy: users[1],
-  //   },
-  // };
+	// console.log(`🟢  result.products `, result.products[0].cartItem)
+	// // Creating CartItem
+	// const users = await Promise.all([
+	//   User.create({ username: 'cody', password: '123' }),
+	//   User.create({ username: 'murphy', password: '123' }),
+	// ]);
+
+	// console.log(`seeded ${users.length} users`);
+	// console.log(`seeded successfully`);
+	// return {
+	//   users: {
+	//     cody: users[0],
+	//     murphy: users[1],
+	//   },
+	// };
 }
 
 /*
@@ -51,17 +65,17 @@ async function seed() {
  The `seed` function is concerned only with modifying the database.
 */
 async function runSeed() {
-  console.log('seeding...');
-  try {
-    await seed();
-  } catch (err) {
-    console.error(err);
-    process.exitCode = 1;
-  } finally {
-    console.log('closing db connection');
-    await db.close();
-    console.log('db connection closed');
-  }
+	console.log('seeding...')
+	try {
+		await seed()
+	} catch (err) {
+		console.error(err)
+		process.exitCode = 1
+	} finally {
+		console.log('closing db connection')
+		await db.close()
+		console.log('db connection closed')
+	}
 }
 
 /*
@@ -70,8 +84,8 @@ async function runSeed() {
   any errors that might occur inside of `seed`.
 */
 if (module === require.main) {
-  runSeed();
+	runSeed()
 }
 
 // we export the seed function for testing purposes (see `./seed.spec.js`)
-module.exports = seed;
+module.exports = seed
