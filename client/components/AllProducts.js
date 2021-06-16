304261e9b509935f1998e79717e79093542a5f5d
Sey <76843977+iseykim@users.noreply.github.com>
2021-06-16 14:18:12 -0400

    Fixed: no need to destructure for useSelector; took out conditional JSX

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -1,32 +1,19 @@
 import React, { useEffect } from 'react'
-import { connect, useDispatch, useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { getProducts } from '../store/products'
 
 const AllProducts = () => {
-  // componentDidMount() {
-  //   try {
-  //     this.props.getInitialProducts()
-  //   } catch (error) {
-  //     console.error(error)
-  //   }
-  // }
   const dispatch = useDispatch()
+  const { products } = useSelector(s => s)
 
   useEffect(() => {
-    console.log('useeffect')
     dispatch(getProducts())
   }, [])
 
-  const { products } = useSelector((state) => {
-    console.log(state)
-    return state.products
-  })
-
-   // const { products } = this.props.products
-
-    return (
-      <div id="allProducts">
-        {products && products.map(product => (
+  return (
+    <div id="allProducts">
+      {products &&
+        products.map(product => (
           <div key={product.id}>
             <div id="productName">{product.name}</div>
             <img src={product.imageUrl} />
@@ -39,20 +26,8 @@ const AllProducts = () => {
             </button>
           </div>
         ))}
-      </div>
-    )
-  }
-
-// const mapState = products => {
-//   return { products }
-// }
-
-// const mapDispatch = dispatch => {
-//   return {
-//     getInitialProducts: () => dispatch(getProducts())
-//   }
-// }
-
-// export default connect(mapState, mapDispatch)(AllProducts)
+    </div>
+  )
+}
 
 export default AllProducts
