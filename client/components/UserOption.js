adb77dfa1d5487667176b270ceffb70b2618a974
Sey <76843977+iseykim@users.noreply.github.com>
2021-06-16 17:22:58 -0400

    Created backend api and basic create/update/delete form

--- client/components/UserOption.js
+++ client/components/UserOption.js
@@ -1,11 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { connect, useDispatch, useSelector } from 'react-redux'
 
 const UserOption = () => {
   const { username } = useSelector(state => state.auth)
+
+  const [state, setState] = useState({
+    name: '',
+    description: '',
+    type: ''
+  })
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log(e.target.value)
+  }
+
+  const handleChange = e => {
+    setState({ ...state, [e.target.name]: e.target.value })
+  }
+
   return (
     <div>
-      <h3>Hooks: Welcome, {username}</h3>
+      <h3>Welcome, {username}</h3>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Name</label>
+        <input
+          onChange={handleChange}
+          name="name"
+          type="text"
+          placeholder="name"
+        />
+        <label htmlFor="description">Description</label>
+        <input
+          onChange={handleChange}
+          name="description"
+          type="text"
+          placeholder="description"
+        />
+        <select onChange={handleChange}>
+          <option value="Whisky">Whisky</option>
+          <option value="Tequila">Tequila</option>
+          <option selected value="Vodka">
+            Vodka
+          </option>
+          <option value="Rum">Rum</option>
+          <option value="Liqueur">Liqueur</option>
+        </select>
+        <button type="submit">Create</button>
+        <input type="text" placeholder="id" />
+        <button type="submit">Update</button>
+        <button type="submit">Delete</button>
+      </form>
     </div>
   )
 }
--- server/api/products.js
+++ server/api/products.js
@@ -23,3 +23,33 @@ router.get('/:productId', async (req, res, next) => {
     next(error)
   }
 })
+
+//POST /api/products/
+router.post('/', async (req, res, next) => {
+  try {
+    res.status(201).send(await Product.create(req.body))
+  } catch (err) {
+    next(err)
+  }
+})
+
+//UPDATE /api/products/
+router.put('/', async (req, res, next) => {
+  try {
+    const currProduct = await Product.findByPk(req.body.productId)
+    res.send(await currProduct.update(req.body))
+  } catch (err) {
+    next(err)
+  }
+})
+
+//DELETE /api/products/
+router.delete('/', async (req, res, next) => {
+  try {
+    const currProduct = await Product.findByPk(req.body.productId)
+    await currProduct.destroy()
+    res.send(currProduct)
+  } catch (err) {
+    next(err)
+  }
+})
