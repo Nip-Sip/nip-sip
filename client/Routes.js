a5a002e508529cd29014717332680356d000ad03
Jason Chen <j.chen2303@gmail.com>
2021-06-16 11:41:52 -0400

    added all products link to nav bar

--- client/Routes.js
+++ client/Routes.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
 import { withRouter, Route, Switch, Redirect } from 'react-router-dom'
 import { Login, Signup } from './components/AuthForm'
+import AllProducts from './components/AllProducts'
 import Home from './components/Home'
 import { me } from './store'
 
@@ -22,12 +23,14 @@ class Routes extends Component {
           <Switch>
             <Route path="/home" component={Home} />
             <Redirect to="/home" />
+            <Route path="/products" component={AllProducts} />
           </Switch>
         ) : (
           <Switch>
             <Route path="/" exact component={Login} />
             <Route path="/login" component={Login} />
             <Route path="/signup" component={Signup} />
+            <Route path="/products" component={AllProducts} />
           </Switch>
         )}
       </div>
---
+++ client/components/AllProducts.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+class AllProducts extends React.Component {
+  render() {
+    return <div>hello world</div>
+  }
+}
+
+export default AllProducts
