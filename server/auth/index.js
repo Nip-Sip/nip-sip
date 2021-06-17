df82a9116b5ab8b1c34570fe6f491885258368da
Sey <76843977+iseykim@users.noreply.github.com>
2021-06-17 15:17:56 -0400

    added boiler plate for auth

--- server/auth/index.js
+++ server/auth/index.js
@@ -4,6 +4,33 @@ const {
 } = require('../db')
 module.exports = router
 
+const requireToken = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization
+    const user = await User.findByToken(token)
+    // TODO: insert a custom error here...
+    req.user = user
+    next()
+  } catch (error) {
+    next(error)
+  }
+}
+
+const requireAdminToken = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization
+    const user = await User.findByToken(token)
+    if (user.isAdmin) {
+      req.user = user
+    } else {
+      throw Error('Not an admin')
+    }
+    next()
+  } catch (error) {
+    next(error)
+  }
+}
+
 router.post('/login', async (req, res, next) => {
   try {
     res.send({ token: await User.authenticate(req.body) })
