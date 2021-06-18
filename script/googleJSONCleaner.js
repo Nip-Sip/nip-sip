b9846b285b22d47281cc4df46a853771f30486fa
Kyle Surma <surmakyl@gmail.com>
2021-06-17 23:25:34 -0500

    fixed Google Sheet seeder

--- script/googleJSONCleaner.js
+++ script/googleJSONCleaner.js
@@ -1,17 +1,31 @@
 const googleJSONCleaner = arr => {
   let cleanArr = []
+  // Google Sheets changes column names to all lowercase when displaying in json. Workaround here is to include column names in row1 & row2 and use first line of data as property name values when iterating through
+  let columnNames = []
+  let firstTimeThrough = true
 
   for (let obj of arr) {
     let newObj = {}
+    let index = 0
     for (let key in obj) {
       if (key.startsWith('gsx$')) {
-        let cleanedKey = key.replace('gsx$', '')
-        newObj[cleanedKey] = obj[key].$t
+        //builds object that will be used for propery names
+        if (firstTimeThrough) {
+          newObj[key] = obj[key].$t
+        } else {
+          let cleanedKey = columnNames[index++]
+          newObj[cleanedKey] = obj[key].$t
+        }
       }
     }
-    cleanArr.push(newObj)
+    //pushes values to columnNames to be used to name the rest of the objects
+    if (firstTimeThrough) {
+      firstTimeThrough = false
+      columnNames = Object.values(newObj)
+    } else cleanArr.push(newObj)
+
   }
-  return cleanArr
+  return cleanArr.slice(1, arr.length)
 }
 
 module.exports = googleJSONCleaner
