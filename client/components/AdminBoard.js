2bc882b7fd109084245cda341cc4f76c3096f833
Sey <iseykim@gmail.com>
2021-06-19 18:08:40 -0400

    Carousel added

--- client/components/AdminBoard.js
+++ client/components/AdminBoard.js
@@ -19,8 +19,50 @@ const AdminBoard = () => {
       <p>Inline style?</p>
       <ThreeDRotation color="secondary" />
       <h2>{admin.length ? 'Admin Eyes Only' : 'Not an admin!'}</h2>
+      <Example />
     </div>
   )
 }
 
 export default AdminBoard
+
+/* Material UI */
+
+// https://www.npmjs.com/package/react-material-ui-carousel
+
+import Carousel from 'react-material-ui-carousel'
+import { Paper, Button } from '@material-ui/core'
+
+function Example() {
+  const items = [
+    {
+      name: 'Random Name #1',
+      description: 'Beach',
+      url: '/img/beach-drink.jpg'
+    },
+    {
+      name: 'Random Name #2',
+      description: 'Pretty drinks',
+      url: '/img/glasses.jpg'
+    }
+  ]
+
+  return (
+    <Carousel>
+      {items.map((item, i) => (
+        <Item key={i} item={item} />
+      ))}
+    </Carousel>
+  )
+}
+
+function Item(props) {
+  return (
+    <Paper>
+      <h2>{props.item.name}</h2>
+      <img style={{ height: '500px' }} src={props.item.url} alt="picture" />
+      <p>{props.item.description}</p>
+      <Button className="CheckButton">Check it out!</Button>
+    </Paper>
+  )
+}
