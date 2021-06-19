32ce515a3ff21b7ba1c18c1be05a1ca2b7e3494e
Sey <iseykim@gmail.com>
2021-06-19 19:08:44 -0400

    Added chart js

--- client/components/AdminBoard.js
+++ client/components/AdminBoard.js
@@ -1,6 +1,45 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getAdminInfo } from '../store/admin'
+import { Pie } from 'react-chartjs-2'
+
+const data = {
+  labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
+    }
+  ]
+}
+
+const PieChart = () => (
+  <>
+    <div className="header">
+      <h1 className="title">Pie Chart</h1>
+    </div>
+    <div style={{ position: 'relative', height: '500px', width: '500px' }}>
+      <Pie data={data} />
+    </div>
+  </>
+)
 
 const AdminBoard = () => {
   const dispatch = useDispatch()
@@ -18,6 +57,7 @@ const AdminBoard = () => {
         {admin.map(user => (
           <div key={user.id}>{user.username}</div>
         ))}
+        <PieChart />
       </div>
     )
   } else {
