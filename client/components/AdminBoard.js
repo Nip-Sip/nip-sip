701f84bf9e8695273cf9edd0699db680a63a440d
Sey <iseykim@gmail.com>
2021-06-19 17:40:08 -0400

    Added Roboto, ui icons

--- client/components/AdminBoard.js
+++ client/components/AdminBoard.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getAdminInfo } from '../store/admin'
+// Material UI Icons
+import { AccessAlarm, ThreeDRotation } from '@material-ui/icons'
 
 const AdminBoard = () => {
   const dispatch = useDispatch()
@@ -13,6 +15,9 @@ const AdminBoard = () => {
   return (
     <div>
       <h1>Logged in but not an Admin</h1>
+      <AccessAlarm color="primary" fontSize="large" />
+      <p>Inline style?</p>
+      <ThreeDRotation color="secondary" />
       <h2>{admin.length ? 'Admin Eyes Only' : 'Not an admin!'}</h2>
     </div>
   )
