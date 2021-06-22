9df6e3ab5fa25d76e9d550ec3b1fc03288e2b5be
Sey <76843977+iseykim@users.noreply.github.com>
2021-06-22 08:57:29 -0400

    find by quantity - see query.js

--- server/db/query.js
+++ server/db/query.js
@@ -1,20 +1,25 @@
 const Sequelize = require('sequelize')
 const {
   db,
-  models: { User, Product }
+  models: { User, Product, CartItem }
 } = require('./index')
 
 async function query() {
   await db.sync()
 
-  const cody = await User.findOne({ where: { username: 'cody' } })
-  const sey = await User.findOne({ where: { username: 'sey' } })
+  // const cody = await User.findOne({ where: { username: 'cody' } })
+  // const sey = await User.findOne({ where: { username: 'sey' } })
+  // const codyProducts = await cody.getProducts()
+  // const seyProducts = await sey.getProducts()
+  // const allProducts = await Product.findAll()
 
-  const codyProducts = await cody.getProducts()
-  const seyProducts = await sey.getProducts()
-  const allProducts = await Product.findAll()
+  // this is useful to find the top 5 items that people bought
+  const c = await CartItem.findAll({
+    limit: 5,
+    order: [['quantity', 'DESC']]
+  })
 
-  console.log(codyProducts.length)
+  console.log(JSON.stringify(c, null, 2))
 }
 
 query()
