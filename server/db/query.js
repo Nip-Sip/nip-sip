ffbb24499f1644653f690d69ca04893f3d5dda57
Sey <76843977+iseykim@users.noreply.github.com>
2021-06-22 17:03:59 -0400

    query prototype done

--- server/db/query.js
+++ server/db/query.js
@@ -29,7 +29,36 @@ async function query() {
 
   // console.log(JSON.stringify(c, null, 2))
 
-  const whisky = await Product.findOne({ where: { category: 'Whisky' } })
+  // User category = queryAll: their top choices in the cartItem table...
+  //
+  //
+  // User query: provide the token, and then provide the body [query];
+  const userFavType = 'Vodka'
+  const variety = await Product.findAll({
+    where: { category: userFavType },
+    limit: 3
+  })
+  // console.log(JSON.stringify(whisky, null, 2))
+  // follow the whisky, enter
+  // for each in the graph database, do a promise all
+  const rows = await Promise.all(
+    graphDatabase[userFavType].map(category =>
+      Product.findAll({
+        where: { category },
+        limit: 2
+      })
+    )
+  )
+
+  console.log(`🟢  rows.length `, rows.length)
+  // combine into an array of at least 5...
+  variety.push(...rows.flat()).sort()
+
+  console.log(`🟢  variety[0] `, variety[0])
+  // console.log(JSON.stringify(userSameRows, null, 2))
+  // r1,r2
+  db.close()
+  //
 }
 
 query()
