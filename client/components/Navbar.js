a7d683d97f971cc4e302b8f44b6aa9a9f5b2215b
Sey <76843977+iseykim@users.noreply.github.com>
2021-06-23 20:24:52 -0400

    clean up navlinks

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -44,7 +44,6 @@ const Navbar = () => {
             <a href="#" onClick={() => dispatch(logout())}>
               Logout
             </a>
-            <NavLink to="/admin">Admin</NavLink>
           </div>
         ) : (
           <div>
