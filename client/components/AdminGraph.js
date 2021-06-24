36a79e9eb35838eb85d591683308614af35d9773
Sey <76843977+iseykim@users.noreply.github.com>
2021-06-23 21:35:03 -0400

    fixed

--- client/components/AdminGraph.js
+++ client/components/AdminGraph.js
@@ -1,78 +1,78 @@
 import React, { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getAdminInfo } from '../store/admin'
-import { Pie, Line } from 'react-chartjs-2'
+// import { Pie, Line } from 'react-chartjs-2'
 
-const data = {
-  labels: ['1', '2', '3', '4', '5', '6'],
-  datasets: [
-    {
-      label: 'Total Revenue',
-      data: [12, 19, 3, 5, 2, 3],
-      fill: false,
-      backgroundColor: 'rgb(255, 99, 132)',
-      borderColor: 'rgba(255, 99, 132, 0.2)'
-    }
-  ]
-}
+// const data = {
+//   labels: ['1', '2', '3', '4', '5', '6'],
+//   datasets: [
+//     {
+//       label: 'Total Revenue',
+//       data: [12, 19, 3, 5, 2, 3],
+//       fill: false,
+//       backgroundColor: 'rgb(255, 99, 132)',
+//       borderColor: 'rgba(255, 99, 132, 0.2)'
+//     }
+//   ]
+// }
 
-const options = {
-  scales: {
-    yAxes: [
-      {
-        ticks: {
-          beginAtZero: true
-        }
-      }
-    ]
-  }
-}
+// const options = {
+//   scales: {
+//     yAxes: [
+//       {
+//         ticks: {
+//           beginAtZero: true
+//         }
+//       }
+//     ]
+//   }
+// }
 
-export const LineChart = () => (
-  <>
-    <div className="header">
-      <h1 style={{ textAlign: 'center' }}>Sales Over Time</h1>
-    </div>
-    <Line data={data} options={options} />
-  </>
-)
+// export const LineChart = () => (
+//   <>
+//     <div className="header">
+//       <h1 style={{ textAlign: 'center' }}>Sales Over Time</h1>
+//     </div>
+//     <Line data={data} options={options} />
+//   </>
+// )
 
-const data2 = {
-  labels: ['Whisky', 'Vodka', 'Tequila', 'Liqeur', 'Rum'],
-  datasets: [
-    {
-      label: '# of Votes',
-      data: [32, 31, 9, 22, 2, 3],
-      backgroundColor: [
-        'orange',
-        'blueviolet',
-        'deeppink',
-        'deepskyblue',
-        'khaki'
-      ],
-      borderColor: ['orange', 'blueviolet', 'deeppink', 'deepskyblue', 'khaki'],
-      borderWidth: 1
-    }
-  ]
-}
+// const data2 = {
+//   labels: ['Whisky', 'Vodka', 'Tequila', 'Liqeur', 'Rum'],
+//   datasets: [
+//     {
+//       label: '# of Votes',
+//       data: [32, 31, 9, 22, 2, 3],
+//       backgroundColor: [
+//         'orange',
+//         'blueviolet',
+//         'deeppink',
+//         'deepskyblue',
+//         'khaki'
+//       ],
+//       borderColor: ['orange', 'blueviolet', 'deeppink', 'deepskyblue', 'khaki'],
+//       borderWidth: 1
+//     }
+//   ]
+// }
 
-export const PieChart = () => (
-  <>
-    <div className="header">
-      <h1 style={{ textAlign: 'center' }}>Top Products</h1>
-    </div>
-    <div
-      style={{
-        position: 'relative',
-        height: '500px',
-        width: '500px',
-        margin: '0 auto'
-      }}
-    >
-      <Pie data={data2} />
-    </div>
-  </>
-)
+// export const PieChart = () => (
+//   <>
+//     <div className="header">
+//       <h1 style={{ textAlign: 'center' }}>Top Products</h1>
+//     </div>
+//     <div
+//       style={{
+//         position: 'relative',
+//         height: '500px',
+//         width: '500px',
+//         margin: '0 auto'
+//       }}
+//     >
+//       <Pie data={data2} />
+//     </div>
+//   </>
+// )
 
 const AdminBoard = () => {
   const dispatch = useDispatch()
@@ -90,7 +90,7 @@ const AdminBoard = () => {
         {admin.map(user => (
           <div key={user.id}>{user.username}</div>
         ))}
-        <PieChart />
+        {/* <PieChart /> */}
       </div>
     )
   } else {
